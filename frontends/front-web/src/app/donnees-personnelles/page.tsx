3a5dcea575489d56a67e53af2d3fb6d43d09d30c
benjamin <benjamin.guelle@gmail.com>
2024-04-10 17:23:23 +0200

    create rgpd

--- frontends/front-web/src/app/donnees-personnelles/page.tsx
+++ frontends/front-web/src/app/donnees-personnelles/page.tsx
@@ -1,10 +1,67 @@
 import { Typography } from '@/components/ui/typography';
+import { Container } from '@/components/ui/container';
+import React from 'react';
+import { ButtonLink } from '@/components/ui/buttonLink';
 
 export default function PersonalData() {
 
   return (
-    <main className={'bg-accent w-full h-full'}>
-      <Typography>Personal data</Typography>
-    </main>
+    <Container className={'space-y-10 min-h-screen py-20'}>
+      <Typography comp={'h1'} weight={'extrabold'} variant={'t-4'} className={'pb-6'}>
+        Politique de Confidentialité de <span className={'font-extrabold'}>franckgaumont.fr</span>
+      </Typography>
+      <Typography comp={'div'} className={'space-y-10 pb-20'}>
+        <div>
+          <h3 className={'font-semibold text-xl'}>Introduction :</h3>
+          <p>{`Chez franckgaumont.fr, nous nous engageons à protéger la confidentialité et 
+            la sécurité des données personnelles que nous recueillons auprès de vous en tant qu'utilisateur de notre site.
+            Cette politique explique comment nous collectons, utilisons et partageons vos informations personnelles.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Quelles informations collectons-nous ? :</h3>
+          <p>{`Nous collectons des informations que vous nous fournissez directement via notre formulaire de contact, 
+          comme votre nom, votre adresse e-mail et tout autre renseignement que vous choisissez de nous fournir.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Comment utilisons-nous vos informations ? :</h3>
+          <p>{`Les informations que nous collectons via le formulaire de contact sont utilisées pour répondre à vos demandes, 
+          gérer notre relation client, et, si vous y consentez, vous envoyer des informations sur nos services.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Partage des informations :</h3>
+          <p>{`Nous ne partageons pas vos données personnelles avec des tiers, sauf si cela est nécessaire pour répondre à vos demandes, se conformer à la loi, ou protéger nos droits.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Vos droits :</h3>
+          <p>{`Conformément au RGPD, vous avez le droit d'accéder, de rectifier, de supprimer et de restreindre le traitement 
+          de vos données personnelles. Vous pouvez également vous opposer au traitement de vos données et avez le droit à la portabilité de vos données.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Sécurité :</h3>
+          <p>{`Nous prenons des mesures de sécurité strictes pour protéger vos données personnelles contre l'accès non autorisé, 
+          la modification, la divulgation ou la destruction.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Modifications de cette politique :</h3>
+          <p>{`NNous pouvons mettre à jour cette politique de confidentialité pour refléter les changements dans nos pratiques d'information. 
+          Si nous apportons des changements importants, nous vous en informerons sur notre site ou par d'autres moyens, comme l'e-mail.`}</p>
+        </div>
+
+        <div>
+          <h3 className={'font-semibold text-xl'}>Contactez-nous :</h3>
+          <p>{`Pour toute question ou préoccupation concernant cette politique ou vos données personnelles, 
+          veuillez nous contacter à eurl.franckgaumont@gmail.com.`}</p>
+        </div>
+
+        <p>Cette Politique de Confidentialité est effective à partir du 10 avril 2024.</p>
+      </Typography>
+      <ButtonLink path={'/'}>{`Retour à l'accueil`}</ButtonLink>
+    </Container>
   );
 }
--- frontends/front-web/src/app/mentions-legales/page.tsx
+++ frontends/front-web/src/app/mentions-legales/page.tsx
@@ -1,19 +1,33 @@
 import { Typography } from '@/components/ui/typography';
 import { Container } from '@/components/ui/container';
 import { ButtonLink } from '@/components/ui/buttonLink';
+import { ContactLink } from '@/components/ui/contactLink';
+import { cn } from '@/lib/utils';
+import { RiPhoneFill } from 'react-icons/ri';
+import React from 'react';
 
 export default function LegalNotice() {
 
   return (
-    <Container className={'space-y-10 h-screen pt-20'}>
+    <Container className={'space-y-10 min-h-screen py-20'}>
       <Typography comp={'h1'} weight={'extrabold'} variant={'t-4'} className={'pb-6'}>Mentions Légales</Typography>
-      <Typography comp={'div'} className={'space-y-10'}>
+      <Typography comp={'div'} className={'space-y-10 pb-20'}>
         <p>En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique,
           il est précisé aux utilisateurs du site https://franckgaumont.fr l’identité des différents intervenants dans
           le cadre de sa réalisation et de son suivi :</p>
-        <div>
+        <div className={'flex flex-col'}>
           <h3 className={'font-semibold text-xl'}>Propriétaire :</h3>
-          <p>Franck Gaumont</p>
+          <p>Eurl Franck Gaumont</p>
+          <ContactLink contactLink={'ADDRESS'} className={'hidden lg:flex'} variant={'black'} weight={'medium'}/>
+          <a href={'tel:0783729832'}
+             className={cn('inline-flex items-center space-x-1 group group-hover:text-primary/75')}>
+            <RiPhoneFill className={'group-hover:text-primary/50'} size={20}/>
+            <Typography weight={'medium'} theme={'primary'} className={'md:text-xl group-hover:text-primary/75'}>
+              07 83 72 98 32
+            </Typography>
+          </a>
+          <ContactLink contactLink={'MAIL'} className={'hidden lg:flex '} variant={'black'}
+                       weight={'medium'}/>
         </div>
         <div>
           <h3 className={'font-semibold text-xl'}>Créateur :</h3>
